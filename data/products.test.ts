fbcb314cf55bf201fc42e4350882d485008fabc9
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-04-28 19:58:51 +0200

    Mock products.json

--- data/products.test.ts
+++ data/products.test.ts
@@ -1,6 +1,6 @@
 import { getBaseProducts, getProduct, getProductVariants, getProductWithVariants } from './products';
 
-// jest.mock('./json/products.json')
+jest.mock('./json/products.json')
 
 test('"getBaseProducts" should returns only base products', () => {
   const baseProducts = getBaseProducts()
@@ -12,12 +12,13 @@ test('"getBaseProducts" should returns only base products', () => {
   expect(baseProducts[2].baseProduct).toStrictEqual('CUFFBEAN')
 
   expect(baseProducts[0].name).toStrictEqual({
+    'en-US': 'en-US Black Baby Short Sleeve Bodysuit with White Logo',
     en: 'Black Baby Short Sleeve Bodysuit with White Logo',
     it: 'Body nero a maniche corte per bebè con logo bianco'
   })
 })
 
-test('"getBaseProducts" should resolves product localization', () => {
+test('"getBaseProducts" should resolves product localization using language', () => {
   const baseProducts = getBaseProducts('it')
 
   expect(baseProducts.length).toStrictEqual(3)
@@ -27,6 +28,20 @@ test('"getBaseProducts" should resolves product localization', () => {
   expect(baseProducts[2].baseProduct).toStrictEqual('CUFFBEAN')
 
   expect(baseProducts[0].name).toStrictEqual('Body nero a maniche corte per bebè con logo bianco')
+  expect(baseProducts[0].description).toStrictEqual('This comfortable baby one-piece is made of 100% combed ring-spun cotton except for heather grey color, which contains polyester. The lap shoulders ensure that the bodysuit can be easily put on and taken off, making for easy changing.')
+})
+
+test('"getBaseProducts" should resolves product localization using language-COUNTRY', () => {
+  const baseProducts = getBaseProducts('en-US')
+
+  expect(baseProducts.length).toStrictEqual(3)
+
+  expect(baseProducts[0].baseProduct).toStrictEqual('BODYBSSS')
+  expect(baseProducts[1].baseProduct).toStrictEqual('BOTT17OZ')
+  expect(baseProducts[2].baseProduct).toStrictEqual('CUFFBEAN')
+
+  expect(baseProducts[0].name).toStrictEqual('en-US Black Baby Short Sleeve Bodysuit with White Logo')
+  expect(baseProducts[0].description).toStrictEqual('en-US This comfortable baby one-piece is made of 100% combed ring-spun cotton except for heather grey color, which contains polyester. The lap shoulders ensure that the bodysuit can be easily put on and taken off, making for easy changing.')
 })
 
 test('"getProduct" should returns the product by provided code', () => {
@@ -47,6 +62,10 @@ test('"getProduct" should resolves product localization', () => {
   expect(product.baseProduct).toStrictEqual('BODYBSSS')
 
   expect(product.name).toStrictEqual('Black Baby Short Sleeve Bodysuit with White Logo')
+})
+
+test('"getProduct" should throws an error when the "code" is not found', () => {
+  expect(() => getProduct('ABCD', 'en')).toThrowError()
 })
 
 test('"getProductVariants" should returns the variants for the provived Product', () => {
