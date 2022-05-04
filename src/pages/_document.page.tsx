c6192f4155006cb69f4ac678c3d328da05481414
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-05-04 13:35:03 +0200

    Add html lang attribute

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -8,8 +8,11 @@ class MyDocument extends Document {
   }
 
   render() {
+    const locale = this.props.__NEXT_DATA__.query.locale as string | undefined
+    const defaultLocale = 'en'
+
     return (
-      <Html>
+      <Html lang={locale || defaultLocale}>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
