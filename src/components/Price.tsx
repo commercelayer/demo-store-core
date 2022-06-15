be384a8f89bedb206fd457ef300e758181ee0111
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-06-15 12:57:26 +0200

    fix(Price): hide discounted price when not available or equal to current price

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -7,7 +7,12 @@ type Props = JSX.IntrinsicElements['div'] & {
 export const Price: React.FC<Props> = ({ price, className = '', ...props }) => {
   return (
     <div className={`flex items-center gap-3 ${className}`} { ...props }>
-      <div className='line-through text-gray-400 font-light text-[0.875em] leading-[1.25em]'>{price.formatted_compare_at_amount}</div>
+      {
+        price.formatted_compare_at_amount &&
+        price.formatted_compare_at_amount !== price.formatted_amount && (
+          <div className='line-through text-gray-400 font-light text-[0.875em] leading-[1.25em]'>{price.formatted_compare_at_amount}</div>
+        )
+      }
       <div className={`text-black font-semibold text-[1em] leading-[1.5em]`}>{price.formatted_amount}</div>
     </div>
   )
