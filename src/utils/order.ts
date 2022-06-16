3bee9e54f616543d6b0dc698743a96786e496956
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-06-16 10:14:41 +0200

    chore: fix shared cart for github pages

--- src/utils/order.ts
+++ src/utils/order.ts
@@ -1,3 +1,4 @@
 import type { ShoppableLocale } from '#i18n/locale'
+import { basePath } from '#next.config'
 
-export const getPersistKey = (locale: ShoppableLocale): string => `country-${locale.country.code}`
+export const getPersistKey = (locale: ShoppableLocale): string => `clayer_order-${basePath ? `${basePath}/` : '/'}${locale.country.code}`
