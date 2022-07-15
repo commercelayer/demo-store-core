e1c7fa02242a1443c12e1233a5cabff78cab6398
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-07-15 12:26:50 +0200

    fix(product): remove background color from product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -14,7 +14,7 @@ export const ProductCard: React.FC<Props> = ({ product, useCommerceLayer = true 
     <div className='group relative'>
       <Link href={getProductUrl(product)}>
         <a>
-          <div className='relative w-full h-80 bg-gray-50 rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1'>
+          <div className='relative w-full h-80 rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1'>
             <img src={product.images[0]} alt={product.name} className='w-full h-full object-center object-contain' />
           </div>
           <h3 className='mt-6 text-base text-black font-medium'>{product.name}</h3>
