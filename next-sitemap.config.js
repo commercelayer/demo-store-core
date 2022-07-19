fd1dfe7e0b417c7775e0c780b8e0f91347846887
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-07-19 17:36:59 +0200

    feat(sitemap): add flag to disable robots

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,24 @@
 // @ts-check
 
 /** @type {import('next-sitemap').IConfig} */
-const sitemapConfig = {
+const sitemapConfig = process.env.DISALLOW_ROBOTS === 'true' ? {
+  siteUrl: process.env.SITE_URL || '',
+  exclude: ['*'],
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*', // Block all crawlers except AdsBot (AdsBot crawlers must be named explicitly)
+        disallow: ['/']
+      },
+      {
+        userAgent: 'AdsBot-Google', // Block AdsBot
+        disallow: ['/']
+      }
+    ]
+  }
+} : {
   siteUrl: process.env.SITE_URL || '',
   sitemapSize: 7000,
   exclude: ['/*/search'],
