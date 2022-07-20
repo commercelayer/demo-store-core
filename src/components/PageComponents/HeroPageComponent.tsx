ca4c9a79324277bc4202c4d808ab1e6a10395cc0
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-07-20 14:40:06 +0200

    feat(images): serve images from Cloudinary

--- src/components/PageComponents/HeroPageComponent.tsx
+++ src/components/PageComponents/HeroPageComponent.tsx
@@ -5,7 +5,7 @@ export const HeroPageComponent: React.FC<{ component: HeroPageComponentType }> =
   return (
     <div key={component.id} data-testid='hero-page-component'>
       <Link href={component.href}>
-        <a className='relative block bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1 lg:aspect-h-1'>
+        <a className='relative block rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1 lg:aspect-h-1'>
           <div className='absolute inset-0 bg-gradient-to-b from-black to-black/0 opacity-50'></div>
           <img alt={component.image.alt} src={component.image.src} className='object-cover w-full h-full' />
           <div className='absolute w-full text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>
