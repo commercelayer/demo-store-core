263b41f333628160509c97ff61a2e19ea1f5f51e
Marco Montalbano <marco.montalbano@commercelayer.io>
2022-07-28 17:10:01 +0200

    perf: memoize 'serverSideTranslations'

--- src/i18n/serverSideTranslations.ts
+++ src/i18n/serverSideTranslations.ts
@@ -1,13 +1,17 @@
 import { fetchData } from '#utils/data'
+import memoize from 'lodash/memoize'
 import lMerge from 'lodash/merge'
 
-export const serverSideTranslations = async (locale: string, path: string = 'locales') => {
-  const [language] = locale.split('-')
+export const serverSideTranslations = memoize(
+  async (locale: string, path: string = 'locales') => {
+    const [language] = locale.split('-')
 
-  const languageDictionary = await fetchData(language, path).catch(_error => ({}))
-  const localeDictionary = await fetchData(locale, path).catch(_error => ({}))
+    const languageDictionary = await fetchData(language, path).catch(_error => ({}))
+    const localeDictionary = await fetchData(locale, path).catch(_error => ({}))
 
-  return {
-    lngDict: lMerge(languageDictionary, localeDictionary)
-  }
-}
+    return {
+      lngDict: lMerge(languageDictionary, localeDictionary)
+    }
+  },
+  (locale, path) => `${locale}-${path}`
+)
