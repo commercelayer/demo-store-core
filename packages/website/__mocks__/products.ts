daad0fb2513d2f8a0d40354be6825c24ee6a717a
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-02-10 15:04:25 +0100

    fix: remove duplicate manual mock found from jest

--- packages/website/__mocks__/products.ts
+++ packages/website/__mocks__/products.ts
@@ -1,4 +1,4 @@
 import { rawDataProducts_schema } from '@commercelayer/demo-store-types'
-import productsJson from '#__mocks__/products.json'
+import productsJson from '#__mocks__/json/products.json'
 
 export default rawDataProducts_schema.parse(productsJson)
