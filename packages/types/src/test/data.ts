9aec7e2bfb4042f4fad00bcba12af896a4776f5f
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-02-14 17:33:53 +0100

    chore: clean-up data.ts

--- packages/types/src/test/data.ts
+++ packages/types/src/test/data.ts
@@ -2,9 +2,9 @@ import at from 'lodash/at'
 import cloneDeep from 'lodash/cloneDeep'
 import set from 'lodash/set'
 import { resolve } from 'path'
-import { suite, Test, test } from 'uvu'
+import { suite, Test } from 'uvu'
 import { equal, is } from 'uvu/assert'
-import { SafeParseReturnType, ZodTypeAny } from 'zod'
+import type { ZodTypeAny } from 'zod'
 import {
   rawDataCatalogs_schema,
   rawDataCountries_schema,
@@ -20,12 +20,21 @@ async function importJson(...paths: string[]) {
   return (await import(resolve(...paths))).default
 }
 
+function green(test: Test) {
+  test('', () => {
+    is(true, true)
+  })
+}
+
 export function testJsonData(path: string) {
-  function green(test: Test) {
-    test('', () => {
-      is(true, true)
-    })
-  }
+  test('catalogs.json', rawDataCatalogs_schema)
+  test('countries.json', rawDataCountries_schema)
+  test('languages.json', rawDataLanguages_schema)
+  test('organization.json', rawDataOrganization_schema)
+  test('pages.json', rawDataPages_schema)
+  test('products.json', rawDataProducts_schema)
+  test('taxonomies.json', rawDataTaxonomies_schema)
+  test('taxons.json', rawDataTaxons_schema)
 
   async function test(filename: `${string}.json`, schema: ZodTypeAny) {
     const suiteName = filename
@@ -40,7 +49,9 @@ export function testJsonData(path: string) {
     } else {
       result.error.issues.forEach(issue => {
         // @ts-expect-error
-        const testName = `Path "${issue.path.join('.')}" - ${issue.message} - Expected type '${issue.expected}'`
+        const expectedType = issue.expected ? ` - Expected type '${issue.expected}'` : ''
+
+        const testName = `Path '${issue.path.join('.')}' - ${issue.message}${expectedType}`
 
         test(testName, () => {
           const actual = at(json, [issue.path[0]!])[0]
@@ -50,107 +61,14 @@ export function testJsonData(path: string) {
           set(
             expected,
             issue.path.slice(1),
-
-            // @ts-expect-error
-            `ERROR ${issue.message} - Expected type '${issue.expected}'`
+            `ERROR - ${issue.message}${expectedType}`
           )
 
-          equal(actual, expected)
+          equal(actual, expected, ' ')
         })
       })
     }
 
     test.run()
-  }
-
-  test('catalogs.json', rawDataCatalogs_schema)
-  test('countries.json', rawDataCountries_schema)
-  test('languages.json', rawDataLanguages_schema)
-  test('organization.json', rawDataOrganization_schema)
-  test('pages.json', rawDataPages_schema)
-  test('products.json', rawDataProducts_schema)
-  test('taxonomies.json', rawDataTaxonomies_schema)
-  test('taxons.json', rawDataTaxons_schema)
-}
-
-export function testJsonData2(path: string) {
-  test(`'catalogs.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'catalogs.json')
-    const result = rawDataCatalogs_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test(`'countries.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'countries.json')
-    const result = rawDataCountries_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test(`'languages.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'languages.json')
-    const result = rawDataLanguages_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test(`'organization.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'organization.json')
-    const result = rawDataOrganization_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test(`'pages.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'pages.json')
-    const result = rawDataPages_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test(`'products.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'products.json')
-    const result = rawDataProducts_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test(`'taxonomies.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'taxonomies.json')
-    const result = rawDataTaxonomies_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test(`'taxons.json' should be properly defined.`, async () => {
-    const json = await importJson(path, 'taxons.json')
-    const result = rawDataTaxons_schema.safeParse(json)
-
-    await expectSafeParseResult(json, result)
-  })
-
-  test.run()
-}
-
-async function expectSafeParseResult<Input, Output>(json: any, result: SafeParseReturnType<Input, Output>) {
-  if (result.success === false) {
-
-    result.error.issues.forEach(issue => {
-      const actual = at(json, [issue.path[0]!])[0]
-
-      const expected = cloneDeep(actual)
-
-      set(
-        expected,
-        issue.path.slice(1),
-
-        // @ts-expect-error
-        `ERROR ${issue.message} - Expected type '${issue.expected}'`
-      )
-
-      // @ts-expect-error
-      equal(actual, expected, `Path "${issue.path.join('.')}" - ${issue.message} - Expected type '${issue.expected}'`)
-    })
   }
 }
